9448206780d5bb1341a3f38b96b746da9be2930c
alptekinkarabacak <alptekinkarabacak@gmail.com>
2023-02-14 15:05:13 +0300

    Testing output has been added

--- UnitTests/main.cpp
+++ UnitTests/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "gtest/gtest.h"
 int main(int, char**) {
+    ::testing::GTEST_FLAG(output) = "xml:test_results.xml";
     ::testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
